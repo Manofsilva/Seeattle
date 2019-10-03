4d823835fec162c1993df82d6f86dae73d6c7716
Cesar Silva <silvacde@gmail.com>
2019-10-02 23:13:50 -0700

    sequelize cli

---
+++ models/itinerary.js
@@ -0,0 +1,9 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references my connection to the DB.
+var sequelize = require("");
+
+// Creates a "Itinerary" model that matches up with DB
