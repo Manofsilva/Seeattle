66177a415460511d51f68b47618ceea87e3bcfb3
Cesar Silva <silvacde@gmail.com>
2019-10-06 17:07:32 -0700

    added additional GET and POST routes

--- routes/attractions-api-routes.js
+++ routes/attractions-api-routes.js
@@ -5,20 +5,20 @@ var db = require('../models');
 // This is to test if the routes work with dummy data
 
 module.exports = function(app) {
-    app.post("/api/attractions", function(req, res) {
-        console.log("testing");
+    // app.post("/api/attractions", function(req, res) {
+    //     console.log("testing");
         // create takes an argument of an object describing the item we want to insert
         // into our table. In this case we just we pass in an object with a text and
         // complete property
-        db.Attraction.create({
-            attractionSite: "DC"
-        }).then(function(dbAttraction) {
+        // db.Attraction.create({
+        //     attractionSite: "DC"
+        // }).then(function(dbAttraction) {
             // We have access to the new attraction as an argument inside of the callback function
-            res.json(dbAttraction);
-        }).catch(function(err){
-            res.json({err: err})
-        })
-    });
+    //         res.json(dbAttraction);
+    //     }).catch(function(err){
+    //         res.json({err: err})
+    //     })
+    // });
 
      // POST route to db for saving a new attraction entered by user
   app.post("/api/attractions/:attractionSite", function(req, res) {
@@ -30,5 +30,28 @@ module.exports = function(app) {
         res.json(dbAttraction);
       });
   });
+
+//   GET route that includes all reviews associated with all tourist attractions
+  app.get("/api/attractions", function(req, res) {
+      db.Attraction.findAll({
+          include: [db.Review]
+      }).then(function(dbAttraction) {
+          res.json(dbAttraction);
+      });
+  });
+
+
+  //   GET route that includes all reviews associated with one tourist attraction
+  app.get("/api/attractions/:review", function(req, res) {
+    db.Attraction.findOne({
+        where: {
+            id: req.params.attractionSite
+        },
+        include: [db.Review]
+    }).then(function(dbAttraction) {
+        res.json(dbAttraction);
+    });
+});
+
 
 }
